bfa8e5bcd223051056f2735c26a8903b82a25848
FilliusMahiae <sergio.mahiarello@gmail.com>
2025-04-07 11:35:41 +0200

    Añadir soft delete al modelo

--- models/Client.js
+++ models/Client.js
@@ -1,23 +1,35 @@
 const mongoose = require("mongoose");
+const mongooseDelete = require("mongoose-delete");
 const Schema = mongoose.Schema;
 
-const ClientSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: { type: String, default: "" },
-  address: {
-    street: { type: String, default: "" },
-    number: { type: Number, default: null },
-    postal: { type: Number, default: null },
-    city: { type: String, default: "" },
-    province: { type: String, default: "" },
+const ClientSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, default: "" },
+    address: {
+      street: { type: String, default: "" },
+      number: { type: Number, default: null },
+      postal: { type: Number, default: null },
+      city: { type: String, default: "" },
+      province: { type: String, default: "" },
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    companyId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   },
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  companyId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  {
+    timestamps: true, // Crea campos createdAt y updatedAt
+  }
+);
+
+// Plugin para soft delete con override de métodos y marca de tiempo
+ClientSchema.plugin(mongooseDelete, {
+  overrideMethods: "all",
+  deletedAt: true,
 });
 
 module.exports = mongoose.model("Client", ClientSchema);
