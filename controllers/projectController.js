bc09509a51274b0268bbbbab971d20f46925ca81
FilliusMahiae <sergio.mahiarello@gmail.com>
2025-04-07 12:27:59 +0200

    Get Projects

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -70,4 +70,48 @@ const updateProject = async (req, res) => {
   }
 };
 
-module.exports = { createProject, updateProject };
+const getProjects = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const companyId = req.user.company?._id;
+
+    const projects = await Project.find({
+      $or: [{ createdBy: userId }, { companyId: companyId }],
+    }).populate("clientId");
+
+    res.json({ projects });
+  } catch (err) {
+    console.error(err);
+    handleHttpError(res, "No se pudieron obtener los proyectos", 400);
+  }
+};
+
+const getProjectById = async (req, res) => {
+  try {
+    const projectId = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(projectId)) {
+      return handleHttpError(res, "ID inválido", 400);
+    }
+
+    const project = await Project.findOne({
+      _id: projectId,
+      $or: [{ createdBy: req.user._id }, { companyId: req.user.company?._id }],
+    }).populate("clientId");
+
+    if (!project) {
+      return handleHttpError(
+        res,
+        "Proyecto no encontrado o no autorizado",
+        404
+      );
+    }
+
+    res.json({ project });
+  } catch (err) {
+    console.error(err);
+    handleHttpError(res, "Error al obtener el proyecto", 400);
+  }
+};
+
+module.exports = { createProject, updateProject, getProjects, getProjectById };
