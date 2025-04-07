25de0a622ebd0a96d7a2fb1f0f79fa08deeb434f
FilliusMahiae <sergio.mahiarello@gmail.com>
2025-04-07 11:55:59 +0200

    Actualizacion de las rutas
    Las rutas daban error por el orden y porque no eran demasiado concretas

--- routes/client.js
+++ routes/client.js
@@ -1,32 +1,44 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
-const { validateClient } = require("../validators/clientValidator");
+const {
+  validateClient,
+  validateClientUpdate,
+} = require("../validators/clientValidator");
+
 const {
   createClient,
-  updateClient,
   getClients,
   getClientById,
+  updateClient,
   softDeleteClient,
   hardDeleteClient,
+  getArchivedClients,
+  recoverClient,
 } = require("../controllers/clientController");
-
-// Crear cliente
-router.post("/", auth, validateClient, createClient);
-
-// Actualizar cliente
-router.put("/:id", auth, validateClient, updateClient);
 
 // Obtener todos los clientes del usuario o su empresa
 router.get("/", auth, getClients);
 
+// Crear un cliente
+router.post("/", auth, validateClient, createClient);
+
+// Obtener todos los clientes archivados
+router.get("/archive", auth, getArchivedClients);
+
 // Obtener un cliente por ID
 router.get("/:id", auth, getClientById);
 
-// Soft delete
-router.delete("/:id", auth, softDeleteClient);
+// Actualizar cliente
+router.put("/:id", auth, validateClientUpdate, updateClient);
 
-// Hard delete con ?hard=true
-router.delete("/:id/force", auth, hardDeleteClient);
+// Eliminar cliente (hard delete)
+router.delete("/:id", auth, hardDeleteClient);
+
+// Archivar cliente (soft delete)
+router.delete("/archive/:id", auth, softDeleteClient);
+
+// Restaurar cliente archivado
+router.patch("/restore/:id", auth, recoverClient);
 
 module.exports = router;
