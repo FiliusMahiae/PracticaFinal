c7c24c9502e1ea44d0d94d9e32c6f1097ef49e01
FilliusMahiae <sergio.mahiarello@gmail.com>
2025-04-08 18:15:27 +0200

    Get All Route

--- routes/deliverynote.js
+++ routes/deliverynote.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 const { validateDeliveryNote } = require("../validators/deliveryNoteValidator");
-const { createDeliveryNote } = require("../controllers/deliveryNoteController");
+const {
+  createDeliveryNote,
+  getDeliveryNotes,
+} = require("../controllers/deliveryNoteController");
 
 // Crear albarán (simple o múltiple)
 router.post("/", auth, validateDeliveryNote, createDeliveryNote);
+
+//Recoger todos los albaranes
+router.get("/", auth, getDeliveryNotes);
 
 module.exports = router;
